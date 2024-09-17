12a7006b32e9b4dfdb0d34cb69daa7fa6235c68a
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-09-17 22:16:07 +0000

    update parametros especiais

--- Sintaxe básica com Python/4 Dominando Funções Python/4.2 Funçoes - p2/parametros_especiais.py
+++ Sintaxe básica com Python/4 Dominando Funções Python/4.2 Funçoes - p2/parametros_especiais.py
@@ -1,5 +1,25 @@
+# Criando função com parametros especiais (Apenas positional)
 def criar_carro(modelo, ano, /, marca, motor):
     print(modelo, ano, marca, motor)
 
+
+criar_carro("Palio", 1999, marca="Fiat", motor="1.0")
+
+
+# ------------------------------------------------
+
+# Apenas Keywords
+def criar_carro(*, modelo, ano, marca, motor):
+    print(modelo, ano, marca, motor)
+
+
+criar_carro(modelo="Palio", ano=1999, marca="Fiat", motor="1.0")
+
+# -------------------------------------------
+
+
+# Keyword e positional
+def criar_carro(modelo, ano, /, *, marca, motor):
+    print(modelo, ano, marca, motor)
 
 criar_carro("Palio", 1999, marca="Fiat", motor="1.0")
