2b877dffee968c37a73d3e5823f188c1172ee984
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-03 20:08:46 -0300

    inicio herança e organização das pastas

---
+++ 03 Programação Orientada a Objetos com Python/2 Aprendendo o Conceito de Herança/1_herança_simples.py
@@ -0,0 +1,23 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+    
+    def ligar motor(self):
+        print("Ligando motor...")
+
+class Motocicleta(Veiculo):
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    pass
+
+
+moto = Motocicleta("preta", "abc-1234", 2)
+print(moto)
