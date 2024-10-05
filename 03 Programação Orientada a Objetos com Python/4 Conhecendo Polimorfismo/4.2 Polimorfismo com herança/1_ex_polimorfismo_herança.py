47b5b5d153a0648aa4efada1c8087226814276e4
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-05 09:14:30 -0300

    add e conclusão pollimorfismo

---
+++ 03 Programação Orientada a Objetos com Python/4 Conhecendo Polimorfismo/4.2 Polimorfismo com herança/1_ex_polimorfismo_herança.py
@@ -0,0 +1,17 @@
+class Passaro:
+    def voar(self):
+        pass
+
+class Pardal(Passaro):
+    def voar(self):
+        print("Voa")
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Não voa")
+
+def plano_de_voo(passaro):
+    passaro.voar()
+
+plano_de_voo(Pardal())
+plano_de_voo(Avestruz())
