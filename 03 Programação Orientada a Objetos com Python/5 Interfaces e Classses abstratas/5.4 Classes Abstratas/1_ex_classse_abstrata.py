c53c0c9506ce9f784311a0b2aba452621da123df
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-06 09:40:38 -0300

    conclusão interfaces e classes abstratas

---
+++ 03 Programação Orientada a Objetos com Python/5 Interfaces e Classses abstratas/5.4 Classes Abstratas/1_ex_classse_abstrata.py
@@ -0,0 +1,51 @@
+from abc import ABC
+
+class ControleRemoto(ABC):
+
+    # Contratos, onde as classes filhas terão que possuir esses métodos
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+    
+    # Todas as classes filhas tem como obrigatoriedade ter a propriedade marca
+    @property
+    @abstractproperty
+    def marca(self):
+        pass
+
+
+class ControleTV(ControleRemoto):
+    def ligar(self):
+        print("Ligando TV...")
+
+    def desligar(self):
+        print("Desligando TV...")
+
+    def marca(self):
+        return "LG"
+
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("Ligando ar condicionado...")
+
+    def desligar(self):
+        print("Desligando ar condicionado...")
+
+    def marca(self):
+        return "SANSUNG"
+
+
+
+controle1 = ControleTV()
+print(controle1.ligar())
+print(controole1.desligar())
+print(controole1.marca())
+
+controle2 = ControleArCondicionado()
+print(controle2.ligar())
+print(controole2.desligar())
+print(controole2.marca())
