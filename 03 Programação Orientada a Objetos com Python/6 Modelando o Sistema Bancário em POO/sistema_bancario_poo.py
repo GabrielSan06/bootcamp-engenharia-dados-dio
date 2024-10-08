8f9d0b1439dc0dab39b86e4d4aeee42024b60e10
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-08 13:32:14 -0300

    alterações sistema bancaario

--- 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
+++ 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
@@ -1,58 +1,100 @@
-from abc import abc
+# Importações necessárias para a elaboração do código
+from abc import ABC, abstractclassmethod, abstractproperty
 
+# Classe pai ou base (cliente)
 class Cliente:
     def __init__(self, endereco):
+
+        # Atributos da classe
         self._endereco = endereco
         self._contas = []
-     
-    def realizar_transacao():
-        pass
 
-    def adicionar_conta():
-        pass
+    # MÉTODOS DA CLASSE CLIENTE
+
+    # Para realizar uma transação é necessário informar a conta e o tipo de transação
+    def realizar_transacao(conta, transacao):
+
+        # Registrando a transação à uma conta
+        Transacao.registrar(conta)
+
+    # Para adicionar a conta, é necessário saber qual a conta que será adicionada
+    def adicionar_conta(conta):
+
+        # Adicionando a conta a lista de contas do cliente
+        self.contas.append(conta)
 
 
+# A pessoa física é uma classe filha de cliente, ou seja, herda tudo o que o cliente tem
 class PessoaFisica(Cliente):
+
+    # Inicializador dos atributos da instância
     def __init__(self, cpf, nome, data_nasc, endereco):
+
+        # Inicializando os atributos da classe pai
         super().__init__(endereco)
+
+        # Atributos adicionais da classe filha
         self._cpf = cpf
         self._nome = nome
         self._data_nasc = data_nasc
         
 
+# Classe pai ou base
 class Conta:
+    # Inicializando os atributos da instância
     def __init__(self, numero, cliente):
+
+        # Atributos da instância
         self._saldo = saldo
         self._mumero = numero
         self._agecia = "0001"
-        self._cliente = cliente
-        self._historico = Historico()
+        self._cliente = cliente # Atribuindo um objeto da classe Cliente
+        self._historico = Historico() # Armazenando o histórico na conta
 
+
+    # Definindo atributos como propriedades para serem acessadas de forma segura (ou seja, retorna apenas o valor, sem permitir a sua alteração)
     @property
     def saldo():
-        pass
+        return self.saldo
 
     @property
     def numero():
-        pass 
+        return self.numero 
     
     @property
     def agencia():
-        pass
+        return self.agencia
     
-    @property
-    def nova_conta():
-        pass
-    
-    def sacar():
+
+    # Definindo um método da classe Conta
+    @classmethod
+    # Passando como parâmetro a classe, o numero da conta e o cliente
+    def nova_conta(cls, numero, cliente):
+        return cls(numero, cliente) # Retornando da classe o numero e o cliente
+
+    # Métodos da instância (objeto criado dentro da classe)
+    def sacar(self, valor):
+        saldo = self._saldo
+        excedeu_saldo = valor > saldo
+
+        # Verificação
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif valor > 0:
+            self._saldo -= valor
+            print("Saque realizado com sucesso!")
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+            # return True
+            
+    def depositar(self, valor):
         pass
 
-    def depositar():
-        pass
 
-    
+# Classe filha da classe conta
 class ContaCorrente(Conta):
     def __init__(self):
+        # atributos da classe
         self._limite = limite
         self._limite_saques = limite_saques
         super().__init__()
@@ -63,7 +105,10 @@ class Historico:
         pass
 
 
-class Transacao:
+class Transacao(ABC):
+
+    # MÉTODO DA CLASSE
+    # Registra o tipo de transação (depósito ou saque)
     def registrar():
         pass
 
