060e17f8d836f7d56b949dac0d3ef88d780a2a47
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-09 12:33:38 -0300

    correção do código do sistema bancário poo

--- 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
+++ 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
@@ -1,11 +1,10 @@
 # Importações necessárias para a elaboração do código
-from abc import ABC, abstractclassmethod, abstractproperty
+from abc import ABC, abstractmethod
 from datetime import datetime
 
 # Classe pai ou base (cliente)
 class Cliente:
     def __init__(self, endereco):
-
         # Atributos da classe
         self._endereco = endereco
         self._contas = []
@@ -13,27 +12,22 @@ class Cliente:
     # MÉTODOS DA CLASSE CLIENTE
 
     # Para realizar uma transação é necessário informar a conta e o tipo de transação
-    def realizar_transacao(conta, transacao):
-
+    def realizar_transacao(self, conta, transacao):
         # Registrando a transação à uma conta
-        Transacao.registrar(conta)
+        transacao.registrar(conta)
 
     # Para adicionar a conta, é necessário saber qual a conta que será adicionada
-    def adicionar_conta(conta):
-
-        # Adicionando a conta a lista de contas do cliente
-        self.contas.append(conta)
+    def adicionar_conta(self, conta):
+        # Adicionando a conta à lista de contas do cliente
+        self._contas.append(conta)
 
 
 # A pessoa física é uma classe filha de cliente, ou seja, herda tudo o que o cliente tem
 class PessoaFisica(Cliente):
-
     # Inicializador dos atributos da instância
     def __init__(self, cpf, nome, data_nasc, endereco):
-
         # Inicializando os atributos da classe pai
         super().__init__(endereco)
-
         # Atributos adicionais da classe filha
         self._cpf = cpf
         self._nome = nome
@@ -44,74 +38,64 @@ class PessoaFisica(Cliente):
 class Conta:
     # Inicializando os atributos da instância
     def __init__(self, numero, cliente):
-
         # Atributos da instância
-        self._saldo = saldo
-        self._mumero = numero
-        self._agecia = "0001"
-        self._cliente = cliente # Atribuindo um objeto da classe Cliente
-        self._historico = Historico() # Armazenando o histórico na conta
+        self._saldo = 0  # Inicializando o saldo como 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente  # Atribuindo um objeto da classe Cliente
+        self._historico = Historico()  # Armazenando o histórico na conta
 
-
-    # Definindo atributos como propriedades para serem acessadas de forma segura (ou seja, retorna apenas o valor, sem permitir a sua alteração)
+    # Definindo atributos como propriedades para serem acessadas de forma segura
     @property
-    def saldo():
-        return self.saldo
+    def saldo(self):
+        return self._saldo
 
     @property
-    def numero():
-        return self.numero 
-    
-    @property
-    def agencia():
-        return self.agencia
-    
+    def numero(self):
+        return self._numero 
 
-    # Definindo um método da classe Conta
+    @property
+    def agencia(self):
+        return self._agencia
+
+    # Método de classe para criar uma nova conta
     @classmethod
-    # Passando como parâmetro a classe, o numero da conta e o cliente
     def nova_conta(cls, numero, cliente):
-        return cls(numero, cliente) # Retornando da classe o numero e o cliente
+        return cls(numero, cliente)  # Retornando da classe o número e o cliente
 
-    # Métodos da instância (objeto criado dentro da classe)
+    # Métodos da instância
     def sacar(self, valor):
-        
-        saldo = self.saldo # Armazenando o valor da propriedade saldo na variável saldo
-        excedeu_saldo = valor > saldo # Armazenando a condição em uma variável
+        saldo = self.saldo  # Armazenando o valor da propriedade saldo na variável saldo
+        excedeu_saldo = valor > saldo  # Armazenando a condição em uma variável
 
         # Verificação
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
-            # Retorna false automaticamente
+            return False  # Retorna false automaticamente
         
-        elif valor > 0:
+        if valor > 0:
             self._saldo -= valor
             print("Saque realizado com sucesso!")
-            return True # Retornando True para encerrar o método aqui
-
+            return True  # Retornando True para encerrar o método aqui
         else:
-            print("Operação falhou! O valor informado é inválido.")
-        
-        # Retornando False para a transação não ser armazenada no histórico em caso de erro
-        return False
-        
+            print("Operação falhou! O valor informado é invál.")
+            return False  # Retornando False para a transação não ser armazenada no histórico em caso de erro
             
     def depositar(self, valor):
-        # verificação se o valor informado é válido
+        # Verificação se o valor informado é válido
         if valor > 0:
-            self._saldo += valor # Incrementando o valor ao atributo saldo
+            self._saldo += valor  # Incrementando o valor ao atributo saldo
             print("Depósito realizado com sucesso!")
+            return True  # Retorna True indicando sucesso
         else:
             print("Operação falhou! O valor informado é inválido.")
-            return False
-
-        return True
+            return False  # Retorna False em caso de erro
 
 
-# Classe filha da classe conta
+# Classe filha da classe Conta
 class ContaCorrente(Conta):
     def __init__(self, numero, cliente, limite=600, limite_saques=3):
-        super().__init__(numero, cliente) # Inicializando os atributos da classe pai
+        super().__init__(numero, cliente)  # Inicializando os atributos da classe pai
 
         # Atributos da classe Conta Corrente
         self.limite = limite
@@ -119,12 +103,12 @@ class ContaCorrente(Conta):
 
     # Funções auxiliares para o método "sacar"
     def _excedeu_limite(self, valor):
-        return valor > self.limite # Retorna um valor booleano
+        return valor > self.limite  # Retorna um valor booleano
 
     def _excedeu_saques(self):
-        # Percorrendo o historico de transações e armazenando em uma variável o número de saques realizados
+        # Percorrendo o histórico de transações e armazenando em uma variável o número de saques realizados
         numero_saques = len(
-            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+            [transacao for transacao in self._historico.transacoes if transacao["tipo"] == Saque.__name__]
         )
         return numero_saques >= self.limite_saques
 
@@ -148,8 +132,9 @@ class ContaCorrente(Conta):
         return f"""\
             Agência:\t{self.agencia}
             Conta:\t\t{self.numero}
-            Titular:\t{self.cliente.nome}
+            Titular:\t{self._cliente.nome}
         """
+
 
 # Classe que armazena as transações realizadas
 class Historico:
@@ -163,36 +148,35 @@ class Historico:
 
     # Método da classe Historico
     def adicionar_transacao(self, transacao):
-
         # Adicionando a transação à lista de transações
-         self._transacoes.append(
+        self._transacoes.append(
             # Formatando para ser adicionado como um dicionário chave:valor
             {
                 "tipo": transacao.__class__.__name__,
                 "valor": transacao.valor,
-                "data_hora": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+                "data_hora": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
             }
         )
 
+
 # Classe abstrata, Transação
 class Transacao(ABC):
-
     @property
-    @abstractproperty
+    @abstractmethod
     def valor(self):
         pass
 
-    @abstractclassmethod
-    # Registra o tipo de transação (depósito ou saque)
+    @abstractmethod
     def registrar(self, conta):
         pass
 
 
 class Deposito(Transacao):
-    def __init__(self):
+    def __init__(self, valor):
         self._valor = valor
 
-    def valor (self, valor):
+    @property
+    def valor(self):
         return self._valor
 
     def registrar(self, conta):
@@ -203,11 +187,13 @@ class Deposito(Transacao):
         if sucesso_transacao:
             conta.historico.adicionar_transacao(self)
 
+
 class Saque(Transacao):
-    def __init__(self):
+    def __init__(self, valor):
         self._valor = valor
-    
-    def valor (self, valor):
+
+    @property
+    def valor(self):
         return self._valor
 
     def registrar(self, conta):
