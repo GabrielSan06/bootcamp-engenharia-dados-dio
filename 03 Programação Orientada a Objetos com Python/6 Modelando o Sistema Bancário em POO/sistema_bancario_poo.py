4c7e4ea2a48fdb37a96b6fcab790b92bf27e88e5
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-10-09 12:28:55 -0300

    Finalização do sistema bancário POO

--- 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
+++ 03 Programação Orientada a Objetos com Python/6 Modelando o Sistema Bancário em POO/sistema_bancario_poo.py
@@ -1,5 +1,6 @@
 # Importações necessárias para a elaboração do código
 from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
 
 # Classe pai ou base (cliente)
 class Cliente:
@@ -74,55 +75,145 @@ class Conta:
 
     # Métodos da instância (objeto criado dentro da classe)
     def sacar(self, valor):
-        saldo = self._saldo
-        excedeu_saldo = valor > saldo
+        
+        saldo = self.saldo # Armazenando o valor da propriedade saldo na variável saldo
+        excedeu_saldo = valor > saldo # Armazenando a condição em uma variável
 
         # Verificação
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
+            # Retorna false automaticamente
         
         elif valor > 0:
             self._saldo -= valor
             print("Saque realizado com sucesso!")
+            return True # Retornando True para encerrar o método aqui
 
         else:
             print("Operação falhou! O valor informado é inválido.")
-            
+        
+        # Retornando False para a transação não ser armazenada no histórico em caso de erro
+        return False
         
             
     def depositar(self, valor):
-        pass
+        # verificação se o valor informado é válido
+        if valor > 0:
+            self._saldo += valor # Incrementando o valor ao atributo saldo
+            print("Depósito realizado com sucesso!")
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+            return False
+
+        return True
 
 
 # Classe filha da classe conta
 class ContaCorrente(Conta):
-    def __init__(self):
-        # atributos da classe
-        self._limite = limite
-        self._limite_saques = limite_saques
-        super().__init__()
+    def __init__(self, numero, cliente, limite=600, limite_saques=3):
+        super().__init__(numero, cliente) # Inicializando os atributos da classe pai
 
+        # Atributos da classe Conta Corrente
+        self.limite = limite
+        self.limite_saques = limite_saques
 
+    # Funções auxiliares para o método "sacar"
+    def _excedeu_limite(self, valor):
+        return valor > self.limite # Retorna um valor booleano
+
+    def _excedeu_saques(self):
+        # Percorrendo o historico de transações e armazenando em uma variável o número de saques realizados
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+        )
+        return numero_saques >= self.limite_saques
+
+    # -------- Fim das funções auxiliares --------
+
+    # Método da classe Conta Corrente
+    def sacar(self, valor):
+        if self._excedeu_limite(valor):
+            print("\nOperação falhou! O valor do saque excede o limite.")
+            return False
+
+        if self._excedeu_saques():
+            print("\nOperação falhou! Número máximo de saques excedido.")
+            return False
+
+        # Caso não tenha excedido o limite nem o número de saques, chama o método sacar da classe pai
+        return super().sacar(valor)
+
+    # Formatação para retorno
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            Conta:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
+
+# Classe que armazena as transações realizadas
 class Historico:
-    def adicionar_transacao():
-        pass
+    def __init__(self):
+        self._transacoes = []
 
+    # Criando a propriedade transações para retornar apenas seu valor quando necessário
+    @property
+    def transacoes(self):
+        return self._transacoes
 
+    # Método da classe Historico
+    def adicionar_transacao(self, transacao):
+
+        # Adicionando a transação à lista de transações
+         self._transacoes.append(
+            # Formatando para ser adicionado como um dicionário chave:valor
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data_hora": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
+
+# Classe abstrata, Transação
 class Transacao(ABC):
 
-    # MÉTODO DA CLASSE
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractclassmethod
     # Registra o tipo de transação (depósito ou saque)
-    def registrar():
+    def registrar(self, conta):
         pass
 
 
 class Deposito(Transacao):
     def __init__(self):
         self._valor = valor
-        super().__init__()
 
+    def valor (self, valor):
+        return self._valor
+
+    def registrar(self, conta):
+        # Registrando na conta o depósito realizado
+        sucesso_transacao = conta.depositar(self.valor)
+
+        # Adicionando o depósito ao histórico
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
 
 class Saque(Transacao):
     def __init__(self):
         self._valor = valor
-        super().__init__()
+    
+    def valor (self, valor):
+        return self._valor
+
+    def registrar(self, conta):
+        # Registrando na conta o saque realizado
+        sucesso_transacao = conta.sacar(self.valor)
+
+        # Adicionando o saque ao histórico
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
